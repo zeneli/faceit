8e8b1dc1d4fbdf0d75f9250f0e7c295784c43c18
Amarildo Zeneli <tud27246@temple.edu>
2017-03-24 10:57:54 -0400

    Add scaling factor to outline radius

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class FaceView: UIView {
 
+    var scale: CGFloat = 0.9  // means 90%
+    
     // Inside draw's coordinate system
     override func draw(_ rect: CGRect) {
         // Emoji face
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2 * scale
         let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)  // or convert(center, from: superview)
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
         path.lineWidth = 5.0
