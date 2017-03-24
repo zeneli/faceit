8e87ac6bfff243337c025ee1efef5d5daf746f3d
Amarildo Zeneli <tud27246@temple.edu>
2017-03-24 10:53:16 -0400

    Draw emoji face outline

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,23 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Amarildo Zeneli on 3/24/17.
+//  Copyright © 2017 Amarildo Zeneli. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    // Inside draw's coordinate system
+    override func draw(_ rect: CGRect) {
+        // Emoji face
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)  // or convert(center, from: superview)
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        UIColor.blue.set()
+        path.stroke()
+    }
+}
