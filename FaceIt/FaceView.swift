fcd9c1b09c63389eeb3dbe0c53c28f895bd8048f
Amarildo Zeneli <tud27246@temple.edu>
2017-03-24 11:08:12 -0400

    Refactor pathForSkull, skullRadius, and skullCenter into private fund and var respectively

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -12,14 +12,23 @@ class FaceView: UIView {
 
     var scale: CGFloat = 0.9  // means 90%
     
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
+    
     // Inside draw's coordinate system
     override func draw(_ rect: CGRect) {
         // Emoji face
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2 * scale
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)  // or convert(center, from: superview)
-        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
-        path.lineWidth = 5.0
         UIColor.blue.set()
-        path.stroke()
+        pathForSkull().stroke()
     }
 }
