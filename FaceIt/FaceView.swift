0c397897d89fb5264fc680de6a72a162661e1271
Amarildo Zeneli <tud27246@temple.edu>
2017-06-02 15:44:25 -0400

    Add some documentation

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -12,13 +12,23 @@ class FaceView: UIView {
 
     var scale: CGFloat = 0.9  // means 90%
     
+    // Skull
+    // computed property for skull; only get property
     private var skullRadius: CGFloat {
-        return min(bounds.size.width, bounds.size.height) / 2 * scale
+        return min(bounds.size.width, bounds.size.height) / 2 * scale  // radius * scale
     }
     private var skullCenter: CGPoint {
         return CGPoint(x: bounds.midX, y: bounds.midY)
     }
     
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
+
+    
+    // Eye
     private enum Eye {
         case left
         case right
@@ -28,9 +38,9 @@ class FaceView: UIView {
         // local function used here only
         func centerOfEye(_ eye: Eye) -> CGPoint {
             let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
-            var eyeCenter = skullCenter
-            eyeCenter.y -= eyeOffset  // move up
-            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset  // add or sub eye offset baed on left or right
+            var eyeCenter = skullCenter  // start eye at center
+            eyeCenter.y -= eyeOffset  // move up a bit
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset  // add or sub eye offset based on left or right eye
             return eyeCenter
         }
         let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
@@ -42,11 +52,6 @@ class FaceView: UIView {
         return path
     }
     
-    private func pathForSkull() -> UIBezierPath {
-        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
-        path.lineWidth = 5.0
-        return path
-    }
     
     // Inside draw's coordinate system
     override func draw(_ rect: CGRect) {
@@ -57,12 +62,14 @@ class FaceView: UIView {
         pathForEye(.right).stroke()
     }
     
-    // Some constants of skull radius to eye and mouth
+    // Some constants of skull radius to eye and skull raidus to mouth
     private struct Ratios {
+        // skull to eye ratios
         static let skullRadiusToEyeOffset: CGFloat = 3
         static let skullRadiusToEyeRadius: CGFloat = 10
+        // mouth skull to ratios
+        static let skullRadiusToMouthOffset: CGFloat = 3
         static let skullRadiusToMouthWidth: CGFloat = 1
         static let skullRadiusToMouthHeight: CGFloat = 3
-        static let skullRadiusToMouthOffset: CGFloat = 3
     }
 }
